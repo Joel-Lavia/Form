b70168ca084527133fdaaf3c7574eda2c5e6db55
Joel-Lavia <joeldisidi8@gmail.com>
2024-01-23 18:54:57 +0100

    integration de la seconde partie de formulaire

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,33 @@ alert(`${data.name} a ete enregistre`)
 }
 
   return (
-      <form onsubmit = {handlesubmit(onSubmit)} action="">
+      <form onsubmit = {handlesubmit(onSubmit)}>
       <div>
         <label>Nom</label>
         <input type="text" name='name' {...register("name")} />
       </div>
 <div>
   <label>Telephone</label>
+  <input name='Phone' type="text" {...register("Phone")}/>
 </div>
+<div>
+  <label>Email</label>
+  <input type="email" name='email' {...register("email") }/>
+</div>
+
+<div>
+  <label htmlFor="">age</label>
+  <input type="number" name="age" {...register("age")} />
+</div>
+<div>
+  <label>Genre</label>
+  <select {...register("gender")}>
+<option value="female">Femme</option>
+<option value="male">Homme</option>
+<option value="other">Autre</option>
+  </select>
+</div>
+<button type='submit'>Enregistrer</button>
       </form>
   )
 }
